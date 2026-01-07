53aca654158cb2583a2b1624b3ec18e0eb7fc424
liorkima5-coder <lior.kima5@gmail.com>
2026-01-07 13:52:24 +0200

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -3,11 +3,12 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+# --- הוסף את השורה הזו ---
+from flask_wtf.csrf import CSRFProtect
 
-# אתחול המשתנים (Instances)
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
-
-# הגדרת ה-Limiter (חובה כי הוא מופיע ב-requirements שלך)
 limiter = Limiter(key_func=get_remote_address)
+# --- וצור את המשתנה הזה ---
+csrf = CSRFProtect()
