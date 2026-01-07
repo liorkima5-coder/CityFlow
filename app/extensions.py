55290246d953471e9f3315b9dc7836c5a20a51f1
liorkima5-coder <lior.kima5@gmail.com>
2026-01-07 13:23:15 +0200

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,21 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+# אתחול המשתנים (Instances)
 db = SQLAlchemy()
 login_manager = LoginManager()
-csrf = CSRFProtect()
 migrate = Migrate()
+
+# הגדרת ה-Limiter (חובה כי הוא מופיע ב-requirements שלך)
 limiter = Limiter(key_func=get_remote_address)
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models import User
-    return User.query.get(int(user_id))
-
-login_manager.login_view = 'auth.login'
-login_manager.login_message = 'אנא התחבר כדי לגשת לעמוד זה.'
-login_manager.login_message_category = 'warning'
